e1d3a6a55b4be941337c2b1cf183562a5de37f8e
iamTazxo <135576844+iamTazxo@users.noreply.github.com>
2023-09-17 03:04:04 -0600

    Add files via upload

---
+++ tarea2/src/main/kotlin/perimetro.kt
@@ -0,0 +1,9 @@
+
+import kotlin.math.*
+
+fun main(){
+    val numero = 5.27 // Define el radio de un círculo
+    val perimetro = Math.PI * 2 * numero // Calcula el área del círculo utilizando π
+
+    println("El perímetro del círculo es: $perimetro")
+}
