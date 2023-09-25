5fa906d0510b1fab18a5cd0c1e4d17f738e7b8ba
iamTazxo <135576844+iamTazxo@users.noreply.github.com>
2023-09-24 18:12:45 -0600

    Add files via upload

---
+++ tarea3/tarea3/src/main/kotlin/pendiente.kt
@@ -0,0 +1,13 @@
+/**
+Balderas Cruz Ismael
+ */
+fun main(){
+    val y_1 = 3f
+    val y_2 = -2f
+    val x_1 = 4f
+    val x_2 = -3f
+
+    val pendiente=(y_2-y_1)/(x_2-x_1)
+
+    println("El valor de la pendiente m es: $pendiente")
+}
